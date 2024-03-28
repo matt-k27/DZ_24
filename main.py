e8aa69b6cc803d1bb31c7da9594402021b20f155
Matt <matt@MacBook-Pro-Matt.local>
2024-03-28 18:11:26 +0100

    prime_generator

---
+++ main.py
@@ -0,0 +1,20 @@
+from inspect import isgenerator
+
+print('DZ24')
+
+
+def prime_generator(end):
+    for number in range(2, end+1):
+        if ((number == 2 or number == 3 or number == 5 or number == 7) or
+                (number % 2 != 0 and number % 3 != 0 and number % 5 != 0 and number % 7 != 0)):
+            yield number
+
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
+
+print('Thank you for using')
